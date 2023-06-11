61a0a3b6ea055e36c0089055b2df092af0341874
Naim Hasan <naimarafat7@gmail.com>
2023-06-11 06:20:50 -0700

    emailFiledDone

--- localStorage.js
+++ localStorage.js
@@ -1,3 +1,5 @@
+//------------------ Name filed works -------------------------
+
 document.getElementById('name-send-btn').addEventListener('click', function () {
     const nameInputFiled = document.getElementById('name-text-filed');
     const nameInputFiledText = nameInputFiled.value;
@@ -5,6 +7,24 @@ document.getElementById('name-send-btn').addEventListener('click', function () {
     nameInputFiled.value = '';
 });
 
+
 document.getElementById('name-btn-delete').addEventListener('click', function () {
     localStorage.removeItem('Name');
+})
+
+
+//------------------------------Email filed works ------------------------
+
+document.getElementById('email-send-btn').addEventListener('click', function () {
+    const emailInputFiled = document.getElementById('email-text-filed')
+    const emailFiledText = emailInputFiled.value;
+    localStorage.setItem('Email', emailFiledText);
+
+    emailInputFiled.value = '';
+
+})
+
+
+document.getElementById('email-btn-delete').addEventListener('click', function () {
+    localStorage.removeItem('Email');
 })
