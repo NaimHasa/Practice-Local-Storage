a765892b0166416b9d0c9a184f02c3927e36593d
Naim Hasan <naimarafat7@gmail.com>
2023-06-11 06:10:37 -0700

    nameFiledDone

--- localStorage.js
+++ localStorage.js
@@ -1,1 +1,10 @@
-console.log('Happy');
+document.getElementById('name-send-btn').addEventListener('click', function () {
+    const nameInputFiled = document.getElementById('name-text-filed');
+    const nameInputFiledText = nameInputFiled.value;
+    localStorage.setItem('Name', nameInputFiledText);
+    nameInputFiled.value = '';
+});
+
+document.getElementById('name-btn-delete').addEventListener('click', function () {
+    localStorage.removeItem('Name');
+})
